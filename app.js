b8f68b8e80136ea6d22c43190ba1356ebd843491
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-20 12:49:20 -0500

    completed the first problem

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi')
+const user = {};
+user.name = 'John';
+console.log(user.name);
+user.surname = 'Smith';
+console.log(user.surname);
+
+user.name = 'Pete';
+console.log(user.name);
+delete user.name;
+
+console.log(user);
